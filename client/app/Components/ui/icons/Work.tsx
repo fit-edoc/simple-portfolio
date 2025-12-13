ec458bead597b4611b0a3b4b9c927cb933866976
fitman <fitmanverma@gmail.com>
2025-12-13 18:16:33 +0530

    again tag

--- client/app/Components/ui/icons/Work.tsx
+++ client/app/Components/ui/icons/Work.tsx
@@ -5,9 +5,9 @@ import { BsCircleFill } from 'react-icons/bs'
 
 const Work = () => {
   return (
-    <div className='h-[25px] max-w-[150px] py-2.5   gap-2 flex justify-center items-center rounded-full bg-green-950'>
-      <p className='text-green-400 text-sm'>OPEN TO WORK</p>
-    <motion.p animate={{opacity:[0,1,0]}} whileHover={{x:[-4,0,4]}} transition={{duration:2,repeat:Infinity,repeatType:"mirror"}}>  <BsCircleFill className=' text-[8px] text-green-200'/></motion.p>
+    <div className='h-[25px] max-w-[130px] py-2.5   gap-2 flex justify-center items-center rounded-full bg-green-200 shadow-[0px_0px_1px_black] dark:bg-green-950'>
+      <p className='text-green-950 dark:text-green-400 text-[12px]'>OPEN TO WORK</p>
+    <motion.p animate={{opacity:[0,1,0]}} whileHover={{x:[-4,0,4]}} transition={{duration:2,repeat:Infinity,repeatType:"mirror"}}>  <BsCircleFill className=' text-[8px] text-green-600 dark:text-green-200'/></motion.p>
       
     </div>
   )
