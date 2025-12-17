99c222c7b76aea5c87adebb37659b28cc1a1eeed
fitman <fitmanverma@gmail.com>
2025-12-17 16:21:25 +0530

    change

--- client/app/Loader/Loader.tsx
+++ client/app/Loader/Loader.tsx
@@ -5,7 +5,7 @@ import {motion} from "motion/react"
 const Loader = () => {
   return (
     <motion.div className='absolute  z-50'  animate={{display:"hidden"}}>
-        <motion.div className="bg-amber-500 " initial={{height:"100vh",width:"100vw"}}  animate={{height:"0vh",display:"hidden"}} transition={{duration:5}}>
+        <motion.div className="" initial={{height:"100vh",width:"100vw",backdropFilter:"blur(10px)"}}  animate={{height:"0vh",display:"hidden",backdropFilter:"blur(0px)"}} transition={{duration:2}}>
            
         </motion.div>
       
